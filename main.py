fd10fc3b49407895c9bc5cfc3bfb5f96be695bd9
darkboi3301 <86971180+darkboi3301@users.noreply.github.com>
2022-04-04 15:04:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import time
 
 
 st.title('Fake Review detector')
-st.header('Detect fake reviews using this tools')
+st.header('Detect fake reviews using this online tool')
 st.text('Made by students of class 12 AM JAIN SCHOOL')
-review = st.text_area("Review To Check")
+review = '"'+st.text_area("Review To Check")+'"'
 
-time.sleep(1)
+
 
 from googlesearch import search
  
@@ -20,7 +20,7 @@ res = ''
 with st.spinner('Wait for it...'): 
     for j in search(review, tld="co.in", num=10, stop=10, pause=2):
         res = res + j
-
+    
 if review.strip() == '':
     st.warning('Review is empty')
 elif res == '':
