3b8ad9f00f7b1434c255a753531e41b2e3084341
darkboi3301 <86971180+darkboi3301@users.noreply.github.com>
2022-04-04 08:25:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ for j in search(review, tld="co.in", num=10, stop=10, pause=2):
     res = ''
     res = res + j
 
-if review =='':
+if review.strip() == '':
     st.subheader('Review is empty')
 elif res == '':
     st.subheader('This review is NOT FAKE')
