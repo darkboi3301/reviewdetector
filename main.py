7ab87c17824e44b1c5e7122ced7152ed6f4d552e
darkboi3301 <86971180+darkboi3301@users.noreply.github.com>
2022-04-04 13:57:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
 
-try:
-    import streamlit as st
-except ImportError:
-    print("No module named 'streamlit' found")
+import streamlit as st
 
-
-try:
-    from googlesearch import search
-except ImportError:
-    print("No module named 'google' found")
 
 import time
 
 
 
 st.title('Fake Review detector')
-st.header('detect fake reviews using this tools')
-st.text('made by students of class 12 AM JAIN SCHOOL')
+st.header('Detect fake reviews using this tools')
+st.text('Made by students of class 12 AM JAIN SCHOOL')
 review = st.text_area("Review To Check")
 
 time.sleep(1)
 
-try:
-    from googlesearch import search
-except ImportError:
-    print("No module named 'google' found")
+from googlesearch import search
  
 # to search
 res = ''
- 
-for j in search(review, tld="co.in", num=10, stop=10, pause=2):
-    res = ''
-    res = res + j
+with st.spinner('Wait for it...'): 
+    for j in search(review, tld="co.in", num=10, stop=10, pause=2):
+        res = ''
+        res = res + j
 
 if review.strip() == '':
-    st.subheader('Review is empty')
+    st.warning('Review is empty')
 elif res == '':
-    st.subheader('This review is NOT FAKE')
+    st.info('This review is not made by a bot and therefore could be Reliable')
 else:
-    st.subheader('this review  is Potentially fake and has been found in the following websites')
+    st.info('this review  is Potentially fake and has been found in the following websites')
     st.write(res)
 
