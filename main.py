79546abe8ef84232e78cba167935f73dee8b3d52
darkboi3301 <86971180+darkboi3301@users.noreply.github.com>
2022-04-04 14:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from googlesearch import search
 res = ''
 with st.spinner('Wait for it...'): 
     for j in search(review, tld="co.in", num=10, stop=10, pause=2):
-        res = ''
         res = res + j
 
 if review.strip() == '':
